68550ebe5aa70affaedba08a85d959678973ec7f
destiny <oghenekumeerieme@gmail.com>
2026-07-14 14:09:33 -0700

    fix: update email parameter from reply_to to replyTo in resend action

--- src/app/lsm/actions.ts
+++ src/app/lsm/actions.ts
@@ -25,7 +25,7 @@ export async function submitLsmRegistration(formData: FormData) {
   try {
     const { data, error } = await resend.emails.send({
       from: `${firstName} ${lastName} <onboarding@resend.dev>`,
-      reply_to: email,
+      replyTo: email,
       to: ['onenightcrusade@gmail.com'], // Using the registered Resend email
       subject: `New LSM Registration from ${firstName} ${lastName}`,
       html: `
